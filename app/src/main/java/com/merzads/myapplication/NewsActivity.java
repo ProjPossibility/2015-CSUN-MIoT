92b25d1d164b2551c2edbf5b11ba9aa8e0a47491
Hunt4Code <nanokid92@gmail.com>
2015-02-15 14:36:08 -0800

    NewsActivity files

--- app/src/main/java/com/merzads/myapplication/NewsActivity.java
+++ app/src/main/java/com/merzads/myapplication/NewsActivity.java
@@ -1,24 +1,65 @@
 package com.merzads.myapplication;
 
+import android.content.Intent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 
 public class NewsActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final ForecastFragment  obj = new ForecastFragment();
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_news);
+        setContentView(R.layout.calendar);
+        final Intent intent = new Intent(NewsActivity.this, ToDoManagerActivity.class);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ForecastFragment())
+                    .commit();
+
+        }
+
+        final String qoute = "works";// view.toString();//"this works";// (String)view.getText();
+
+
+
+        final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                // button.setText("pushed");
+                startActivity(intent);
+                //          speakText(qoute);
+            }
+        });
+
+
+
+
+
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_news, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -36,4 +77,10 @@ public class NewsActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
 }
